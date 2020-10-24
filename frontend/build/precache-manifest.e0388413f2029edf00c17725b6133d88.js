b11b5412a9e1d1da59125b05e0bf19bce0c1b98a
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-10-24 14:21:21 -0700

    readme updated, build removed from gitignore

---
+++ frontend/build/precache-manifest.e0388413f2029edf00c17725b6133d88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b798d31a37728432941ad4503c13ef11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63347a5f6af2877e837a",
+    "url": "/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "0f4b5794aa96ceedc22c",
+    "url": "/static/js/2.866d80c2.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.866d80c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63347a5f6af2877e837a",
+    "url": "/static/js/main.a680be93.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
