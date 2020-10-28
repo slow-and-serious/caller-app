fcd140e1e22d05211850992ca73919829af051c8
will-ing <will-ing@github.com>
2020-10-27 19:36:50 -0700

    fixed bug on posting in DRF

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -5,10 +5,10 @@ from .models import Notification
 class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
-        fields = ('id',
-                  'start_date_time',
-                  'end_date_time',
-                  'message',
-                  'notification_type',
-                  'user_response')
+        fields = (
+            'start_date_time',
+            'end_date_time',
+            'message',
+            'notification_type',
+            'user_response')
         model = Notification
