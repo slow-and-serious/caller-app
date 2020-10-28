28a0a0c789b68c710588096af9963020a337d214
Lee-Roy King <leeroywking@gmail.com>
2020-10-27 21:08:35 -0700

    removed a lot of dead code and moved env variables into .env file

--- twilioHandler/twilio/example.py
+++ twilioHandler/twilio/example.py
@@ -1,43 +1,22 @@
 
 from twilio.rest import Client
-import environ
 from pathlib import Path
 from urllib.parse import urlencode
-
-# BASE_DIR = Path(__file__).resolve().parent.parent
-# env = environ.Env(
-#     # set casting, default value
-#     DEBUG=(bool, False),
-# )
-# # reading .env file
-# environ.Env.read_env()
-
-
-def env(key):
-    map = {
-        "TWILIO_SID": "AC42f3eb6dc4abfe728e13a0401a391c5f",
-        "TWILIO_AUTH": "6be2dc11b46e0a1e67f1bd42c278007f",
-        "TWILIO_NUMBER": "+13342315571",
-        "DEPLOYED_URL": "http://27a3e8e40b76.ngrok.io"
-    }
-    return map[key]
-
+from server import settings
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
 def make_call(call_num, message):
     message = urlencode({"message": message})
-    account_sid = env("TWILIO_SID")
-    auth_token = env("TWILIO_AUTH")
-    base_url = env("DEPLOYED_URL")
+    account_sid = settings.TWILIO_SID
+    auth_token = settings.TWILIO_AUTH
+    base_url = settings.DEPLOYED_URL
     client = Client(account_sid, auth_token)
     call = client.calls.create(
         url=f"{base_url}/twilio/maketwiml?{message}",
         to=call_num,
-        from_=env("TWILIO_NUMBER"),
+        from_=settings.TWILIO_NUMBER
     )
-
-
 
 
 if __name__ == "__main__":
