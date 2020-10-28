cd75f012b7876e3f6f8efee9c9d6ca53e69331f1
Lee-Roy King <leeroywking@gmail.com>
2020-10-27 20:53:49 -0700

    removed dead code mkII

--- server/settings.py
+++ server/settings.py
@@ -146,11 +146,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ],
 
-    # 'DEFAULT_RENDERER_CLASSES': [
-    #     'rest_framework_xml.renderers.XMLRenderer',
-    #     'rest_framework.renderers.JSONRenderer',
-    #     'rest_framework.renderers.BrowsableAPIRenderer',
-    # ]
 }
 
 # Custom user
