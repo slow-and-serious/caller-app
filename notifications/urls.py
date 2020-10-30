7947c8c150b2b3470f5f5acb7a32d29724492bef
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-10-30 14:28:28 -0700

    notification history and rotation history routes added

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import login_test, PostDetail, PostList, start_rotation
-
+from .views import (PostDetail, PostList, login_test, notification_history,
+                    rotation_history, start_rotation)
 
 urlpatterns = [
     path('start-rotation', start_rotation, name='start-rotation'),
+    path('notification-history', notification_history,
+         name='notification-history'),
+    path('rotation-history', rotation_history,
+         name='rotation_history'),
     path('', PostList.as_view(), name='list'),
     path('', PostDetail.as_view(), name='detail'),
     path('test', login_test, name='test'),
