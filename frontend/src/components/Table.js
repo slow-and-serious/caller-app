284a86ebada66ef2f14a72f47965b9837f3f56cd
Richard Whitehead <damian253@me.com>
2020-10-31 17:09:06 -0700

    added condtitional

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -7,8 +7,10 @@ import {
   TableHead,
   TableRow,
 } from "@material-ui/core";
+import { green, red } from "@material-ui/core/colors";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
+
 
 const useStyles = makeStyles({
   table: {
@@ -17,6 +19,10 @@ const useStyles = makeStyles({
 });
 
 export default function BasicTable(props) {
+  function D() { return false }
+  const conditional = props.conditional || D
+
+
   const classes = useStyles();
   const { rows, headers } = props;
   const fieldNames = Object.keys(rows[0]);
@@ -35,9 +41,12 @@ export default function BasicTable(props) {
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.id}>
-              {fieldNames.map((field) => (
-                <TableCell align="left">{row[field]}</TableCell>
-              ))}
+              {fieldNames.map((field) => {
+                return conditional(row[field]) ?
+                  <props.effect text={row[field]} />
+                  :
+                  <TableCell align="left">{row[field]}</TableCell>
+              })}
             </TableRow>
           ))}
         </TableBody>
@@ -45,3 +54,5 @@ export default function BasicTable(props) {
     </TableContainer>
   );
 }
+
+// {<TableCell className = {row.user_response==='ACCEPT'? classes.green : row.user_response==='DECLINE'? classes.red : null} align="left">{row.user_response}
