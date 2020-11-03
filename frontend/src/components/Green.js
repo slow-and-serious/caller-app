2e6846d8f36734cee8381a4ebb64cd3cff00e9e0
Richard Whitehead <damian253@me.com>
2020-11-02 21:18:20 -0800

    added css turning the accept response green

--- frontend/src/components/Green.js
+++ frontend/src/components/Green.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import './Green.css'
 export default function Green(props){
-  return <em>{props.text}</em>
+  return <span className="GreenText">{props.text}</span>
 }
 
 // {<TableCell className = {row.user_response==='ACCEPT'? classes.green : row.user_response==='DECLINE'? classes.red : null} align="left">{row.user_response}
